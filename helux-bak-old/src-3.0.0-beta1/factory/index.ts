01eb84b50a24cbe60f52641ffe213200274455c1
fantasticsoul <zhongzhengkai@gmail.com>
2023-10-31 21:07:14 +0800

    feat: del

--- helux-bak-old/src-3.0.0-beta1/factory/index.ts
+++
@@ -1,5 +0,0 @@
-export { derive } from './createDerived';
-export { deriveAsync } from './createDerivedAsync';
-export { deriveTask } from './createDerivedTask';
-export { createShared, share } from './createShared';
-export { watch } from './createWatch';
--- helux-bak-old/src-hot-reload-support/factory/index.ts
+++
@@ -1,5 +0,0 @@
-export { createDerived } from './createDerived';
-export { createDerivedAsync } from './createDerivedAsync';
-export { createDerivedTask } from './createDerivedTask';
-export { createDeepShared, createShared } from './createShared';
-export { createWatch } from './createWatch';
